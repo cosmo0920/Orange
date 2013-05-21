40b613cceba4dda87bdaec372e04199fdd9729e9
Hatake Hiroshi <hhatake@aiming-inc.com>
2013-05-21 18:34:13 +0900

    first commit

---
+++ config/initializers/secret_token.rb
@@ -0,0 +1,12 @@
+# Be sure to restart your server when you modify this file.
+
+# Your secret key is used for verifying the integrity of signed cookies.
+# If you change this key, all old signed cookies will become invalid!
+
+# Make sure the secret is at least 30 characters and all random,
+# no regular words or you'll be exposed to dictionary attacks.
+# You can use `rake secret` to generate a secure secret key.
+
+# Make sure your secret_key_base is kept private
+# if you're sharing your code publicly.
+Orange::Application.config.secret_key_base = '49270b68fbbc30632a6f19bdd114c9c4c7bb2dcccede903001955df4a91dca797abf94148318b1817f66b76ab28110c01d30f7fdfeef29dc61bf08e737f0d4c1'
