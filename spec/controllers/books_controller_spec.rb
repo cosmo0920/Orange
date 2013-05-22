22bd3fd26019fd257b29c5862f137dc8873fe76d
Hatake Hiroshi <hhatake@aiming-inc.com>
2013-05-22 18:12:30 +0900

    impliments listing books

--- spec/controllers/books_controller_spec.rb
+++ spec/controllers/books_controller_spec.rb
@@ -3,11 +3,13 @@ require 'spec_helper'
 describe BooksController do
 
   describe "GET 'index'" do
+		let!(:books) { FactoryGirl.create_list(:book, 2) }
+
 		before do
       get 'index'
 		end
 
-    it { response.should be_success }
+    it { assigns(:books).should eq books }
   end
 
 end
--- spec/features/books_spec.rb
+++ spec/features/books_spec.rb
@@ -1,10 +1,11 @@
 require 'spec_helper'
 
 describe BooksController do
+	let!(:book) { FactoryGirl.create(:book) }
 	before do
-		FactoryGirl.create(:book)
 		visit books_path
 	end
 
-	it { page.should have_content 'Books#index' }
+	it { page.should have_selector('table tr') }
+	it { page.should have_content(book.title) }
 end
