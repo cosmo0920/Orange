d20bc4e6d36d1cf71778b9a04879c5a3a7a44df5
Hatake Hiroshi <hhatake@aiming-inc.com>
2013-05-22 12:46:11 +0900

    add books_controller

---
+++ db/migrate/20130522032846_create_books.rb
@@ -0,0 +1,10 @@
+class CreateBooks < ActiveRecord::Migration
+  def change
+    create_table :books do |t|
+			t.string :title, null: false
+			t.text :description,  null: false
+			t.string :isbn,  limit:13, null: false
+      t.timestamps
+    end
+  end
+end
