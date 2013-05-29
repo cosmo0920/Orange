5f3440e0a79e8c10fb16cd8f15629e0da4df1674
Hatake Hiroshi <hhatake@aiming-inc.com>
2013-05-29 14:29:16 +0900

    add isbn10 and isbn13 length validater

--- app/models/book.rb
+++ app/models/book.rb
@@ -1,3 +1,11 @@
 class Book < ActiveRecord::Base
   paginates_per 10
+  #ISBN validator
+  validate :length_isbn
+
+  def length_isbn
+    unless isbn.length == 10 || isbn.length == 13
+      errors.add(:isbn, "ISBN length must be 10 or 13")
+    end
+  end
 end
