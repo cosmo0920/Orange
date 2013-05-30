3607b1c3e7d5fb93427f6cd89e11382a288dc9cb
Hatake Hiroshi <hhatake@aiming-inc.com>
2013-05-30 18:11:05 +0900

    add validation of image_url length and modify book_spec

--- spec/models/book_spec.rb
+++ spec/models/book_spec.rb
@@ -14,24 +14,27 @@ describe Book do
 			it { subject.count.should eq 2 }
 		end
 	end
+  describe "validation" do
+    describe "isbn_is_appropriate" do
+      context "valid ISBN13" do
+	  	  let(:book) { FactoryGirl.build(:book, isbn: "9784797363821") }
+	  	  subject { book }
+        it { subject.should be_valid }
+      end
 
-  describe "isbn_is_appropriate" do
-    context "valid ISBN13" do
-		  let(:book) { FactoryGirl.build(:book, isbn: "9784797363821") }
-		  subject { book }
-      it { subject.should be_valid }
+      context "vaild ISBN10" do
+	  	  let(:book) { FactoryGirl.build(:book, isbn: "4894717115") }
+	  	  subject { book }
+        it { subject.should be_valid }
+      end
+
+      context "InValid ISBN" do
+	  	  let(:book) { FactoryGirl.build(:book, isbn: Forgery::Basic.text(exactly: 12).to_s) }
+	  	  subject { book }
+        it { subject.should_not be_valid }
+      end
     end
 
-    context "vaild ISBN10" do
-		  let(:book) { FactoryGirl.build(:book, isbn: "4894717115") }
-		  subject { book }
-      it { subject.should be_valid }
-    end
-
-    context "InValid ISBN" do
-		  let(:book) { FactoryGirl.build(:book, isbn: Forgery::Basic.text(exactly: 12).to_s) }
-		  subject { book }
-      it { subject.should_not be_valid }
-    end
+    it { should ensure_length_of(:image_url).is_at_most(2048) }
   end
 end
