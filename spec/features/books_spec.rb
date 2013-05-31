10e2d346de61c5ba51d1ab6f255ebd1c9a5f67c5
Hatake Hiroshi <hhatake@aiming-inc.com>
2013-05-31 12:56:39 +0900

    add view paginate book

--- spec/features/books_spec.rb
+++ spec/features/books_spec.rb
@@ -1,11 +1,29 @@
 require 'spec_helper'
 
 describe BooksController do
-	let!(:book) { FactoryGirl.create(:book) }
-	before do
-		visit books_path
-	end
+  context "page should contains book title" do
+    let!(:book) { FactoryGirl.create(:book) }
+    before do
+      visit books_path
+    end
 
-	it { page.should have_css('#booklist') }
-	it { page.find('#booklist').should have_content(book.title) }
+    it { page.should have_css('#booklist') }
+    it { page.find('#booklist').should have_content(book.title) }
+  end
+  context "paginator" do
+    context "page should have nav seletor when book.count > 10" do
+      let!(:book) { FactoryGirl.create_list(:book,12) }
+      before do
+        visit books_path
+      end
+      it { page.should have_selector('nav') }
+    end
+    context "page should not have nav seletor when book.count <= 10" do
+      let!(:book) { FactoryGirl.create_list(:book,10) }
+      before do
+        visit books_path
+      end
+      it { page.should_not have_selector('nav') }
+    end
+  end
 end
