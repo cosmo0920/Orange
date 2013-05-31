77b5ddb4b2f546722163ee27d7c065d225b9e4a2
Hatake Hiroshi <hhatake@aiming-inc.com>
2013-05-31 19:10:08 +0900

    add method stubs and add link to new_book_path

--- app/controllers/books_controller.rb
+++ app/controllers/books_controller.rb
@@ -2,4 +2,12 @@ class BooksController < ApplicationController
   def index
     @books = Book.all.page params[:page]
   end
+
+  def new
+    @books = Book.new
+  end
+
+  def create
+    redirect_to books_path
+  end
 end
