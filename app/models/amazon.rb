e8777788fbd1a58430e8fce2a8a4a1ce2b72ed98
TAKAHASHI Kazunari <ktakahashi@aiming-inc.com>
2013-06-06 11:13:24 +0900

    refactor amazon.rb

--- app/models/amazon.rb
+++ app/models/amazon.rb
@@ -8,33 +8,32 @@ class Amazon
   end
 
   def book_title
-    return @book_title unless @book_title.blank?
+    @book_title ||= book_title_lookup(@client, @isbn, @isbn_type)
+  end
 
-    isbn, isbn_type = @isbn, @isbn_type #TODO: block内で利用できない.スコープを確認
+  def book_image_url
+    @book_image_url ||= book_image_url_lookup(@client, @isbn, @isbn_type)
+  end
 
-    response = @client.item_lookup do
+  private
+  def book_title_lookup(client, isbn, isbn_type)
+    response = client.item_lookup do
       category 'Books'
       id isbn
       id_type isbn_type
     end
 
-    @book_title = response.item.title
-    @book_title
+    response.item.title
   end
 
-  def book_image_url
-    return @book_image_url unless @book_image_url.blank?
-
-    isbn, isbn_type = @isbn, @isbn_type #TODO: block内で利用できない.スコープを確認
-
-    response = @client.item_lookup do
+  def book_image_url_lookup(client, isbn, isbn_type)
+    response = client.item_lookup do
       category 'Books'
       response_group 'Images'
       id isbn
       id_type isbn_type
     end
 
-    @book_image_url = response.item.small_image.url
-    @book_image_url
+    response.item.small_image.url
   end
 end
