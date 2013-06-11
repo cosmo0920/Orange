a9d65d67187ad7536b4c81be51fcaa2c659ce6ef
Hatake Hiroshi <hhatake@aiming-inc.com>
2013-06-11 17:26:51 +0900

    use named_let

--- spec/models/amazon_spec.rb
+++ spec/models/amazon_spec.rb
@@ -5,13 +5,14 @@ unless ENV['CI']
     let(:isbn) { '9784274068669' }
     let(:isbn_type) { 'EAN' }
     let(:amazon) {  Amazon.new(isbn, isbn_type) }
+    named_let(:image_url) { 'http://ecx.images-amazon.com/images/I/51Gibg-iYTL._SL75_.jpg' }
 
     describe "#book_title" do
       it { expect(amazon.book_title).to match(/Rails/) }
     end
 
     describe "#book_image_url" do
-      it { expect(amazon.book_image_url).to eq('http://ecx.images-amazon.com/images/I/51Gibg-iYTL._SL75_.jpg') }
+      it { expect(amazon.book_image_url).to eq(image_url) }
     end
   end
 end
